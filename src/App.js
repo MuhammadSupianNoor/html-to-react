6dad51419ff3525e069742bc06c1c167af43c524
supian <muhammadsupiannoor.jp@gmail.com>
2021-05-08 13:14:05 +0800

    belajar react 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "scenes/Homepade";
+import "assets/css/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
+}
+export default App;
